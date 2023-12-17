a8c6d08603e66b95ccdc12bb058f0ac72e7a336c
SK Hasibul Hossain <86801974+sk-hasibul-hossain@users.noreply.github.com>
2023-12-17 09:06:10 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,28 @@ import '../styles/App.css';
 import { Loader } from './Loader';
 import { PhotoFrame } from './PhotoFrame';
 const App = () => {
-  
+  const[id,setId] = React.useState(null);
+  const[photo,setPhoto] = React.useState(null);
+  console.log(photo)
+  React.useEffect(()=>{
+    if(id === 0) return;
+    // console.log("Hi Alok")
+      setPhoto(null)
+      fetch(`https://jsonplaceholder.typicode.com/photos/${id}`).then((response)=>{
+        return response.json()
+      }).then(result=>{
+        setPhoto(result)
+      })
+  },[id])
+  return (
+    <div className="App">
+      <input type="number" onChange={(event)=>{
+        setId(event.target.value)
+      }}/>
+      {photo && <PhotoFrame title={photo.title} url={photo.url} />}
+      {id && !photo && <Loader/>}
+    </div>
+  );
 }
 
 
