decd4abd27bffcaf366767aec581a4b518cefd7c
SK Hasibul Hossain <86801974+sk-hasibul-hossain@users.noreply.github.com>
2023-12-17 09:15:44 +0530

    Update PhotoFrame.js

--- src/components/PhotoFrame.js
+++ src/components/PhotoFrame.js
@@ -1,5 +1,11 @@
 import React from 'react';
 
-export const PhotoFrame = () => {
-   
+export const PhotoFrame = ({title,url}) => {
+   // if(!title) return;
+  return(
+    <div className="photoframe">
+      <img src={url}/>
+      <div className="caption">{title}</div>
+    </div>
+  )
 }
